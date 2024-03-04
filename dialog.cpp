0229d7fd16237b1fd8b5b2125bcca34ec8261c2d
Viktor Tsvigo <11_70@bk.ru>
2024-03-04 10:46:16 +0300

    Пн 04 мар 2024 10:46:31 MSK

--- dialog.cpp
+++ dialog.cpp
@@ -61,24 +61,35 @@ Dialog::Dialog(QWidget *parent)
 /// тут если файл с ошибкой существует читаем ошибку из него NOTE: остановился
 ///    #include <QFileInfo>
 //(не забудьте добавить соответствующий #include-оператор)
+    // TODO: После настройки надо включить (раскомментировать) загрузку из файла error
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+    if (fileExists("/home/viktor/my_projects_qt_2/cycle_of_distinguishing_a_one_with_vectors_GUI/error.txt")==true )
+    {;;
+        // считываем ошибку из файла
+        QFile file("/home/viktor/my_projects_qt_2/cycle_of_distinguishing_a_one_with_vectors_GUI/error.txt");
+        if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+            return;
+        QByteArray line = file.readLine();
+         file.close();
+        bool ok;
+        variable_error=line.toLongLong (&ok,10);//.toLongLong ();
+    }
     
-//    if (fileExists("/home/viktor/my_projects_qt_2/cycle_of_distinguishing_a_one_with_vectors_GUI/error.txt")==true )
-//    {;;
-//        // считываем ошибку из файла
-//        QFile file("/home/viktor/my_projects_qt_2/cycle_of_distinguishing_a_one_with_vectors_GUI/error.txt");
-//        if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
-//            return;
-//        QByteArray line = file.readLine();
-//         file.close();
-//        bool ok;
-//        variable_error=line.toLongLong (&ok,10);//.toLongLong ();
-//    }
-//else
-        
+else // если файла с ошибкой нет
+// если хотим включить настройку сопротивлений синапсов блок отсюда и выше до 66 строки закомментируем
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///         
     variable_error     =   1073741824-list_of_neurons[200] ; // 1843778052
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///     
+// тут видимо считать из файла 201 нейрон   
+    
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////   
     if //(eto_ne_1==true)
-        (list_of_neurons[200]>=1073741824 ) // TODO: поменять логику на противоположную
+    //    (list_of_neurons[200]>=1073741824 )
+        ( variable_error <=0)
+    // если  ошибка <= 0
+    // NOTE: УжЕ НЕ НАДО поменять логику на противоположную
     // сейчас неправильно - надо уменьшить решающий 201 нейрон. Уменьшать надо уменьшая на 1 синапсы. Сейчас у меня несоответствие решающего
     // 201 нейрона и переменной ошибки. 201 нейрон меняется с ошибки на истину на 1073741824 а ошибка на 0 то есть соответствующая
     // ошибка меньше 201 нейрона на 1073741824? Надо изменить либо ошибку либо 201 нейрон. Оставим нейрон 1843778052 и высчитаем ошибку =
@@ -100,13 +111,14 @@ Dialog::Dialog(QWidget *parent)
     std::cout << "Variable error = 1073741824-list_of_neurons[200] = " << 1073741824-list_of_neurons[200]<< std::endl;
     std::cout << "list_of_neurons[200]  = " << list_of_neurons[200]  << std::endl;
 b:
+    // NOTE: решение
     ////////////////////////////////////////////// Solution function ////////////////////////////////////////////////////////////
     for ( var = 100; var < 200; ++var) // This is the range of neurons
     {
         for (int neuron_index = 0, synapse_index = 0;   neuron_index < 200, synapse_index < 10100;   ++neuron_index, synapse_index = synapse_index + 100)
         
         {
-            //FIXME: sigpe арифметическое исключение:
+            //FIXME: sigpe арифметическое исключение: (СИНАПСЫ бЫЛИ 0 ДЕление на 0)
             list_of_neurons[var]=list_of_neurons[var]+  (list_of_neurons[neuron_index]/ list_of_synapses[synapse_index]);    
         }
     }
@@ -179,11 +191,26 @@ d:
             QString::number(variable_error); // Посылаем строку в поток для записи
         fileOut.close(); // Закрываем файл
     }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    fstream file2;
+    file2.open("/home/viktor/my_projects_qt_2/cycle_of_distinguishing_a_one_with_vectors/201.txt",ios_base::out);
+    
+
+    {
+
+        file2<< list_of_neurons[200];
+    }
+    
+    file2.close();       
+// записываем решающий нейрон. В принципе можно не записывать.
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////   
     if //(eto_ne_1==true)
-        (list_of_neurons[200]>=1073741824 ) 
+     //   (list_of_neurons[200]>=1073741824 )
+
+            ( variable_error <=0)
     {ui->label->setText("Программа считает что это 1.");} // меняем на это 1
-    else {        ui->label->setText("Программа считает что это не 1.");    } //  меняем на это не 1  
+    else {        ui->label->setText("Программа считает что это не 1.");    } //  меняем на это не 1
+        
 }
 
 Dialog::~Dialog()
@@ -195,102 +222,41 @@ Dialog::~Dialog()
 void Dialog::on_pushButton_clicked()
 {
     //
-   variable_error=1;
-    // создать файл и записать туда ошибку? Потом перезапустить программу?
+//   variable_error=1;
+//    // создать файл и записать туда ошибку? Потом перезапустить программу?
    
-   QFile fileOut("/home/viktor/my_projects_qt_2/cycle_of_distinguishing_a_one_with_vectors_GUI/error.txt"); // Связываем объект с файлом fileout.txt
-   if(fileOut.open(QIODevice::WriteOnly | QIODevice::Text)) 
-   { // Если файл успешно открыт для записи в текстовом режиме
-       QTextStream writeStream(&fileOut); // Создаем объект класса QTextStream
-       // и передаем ему адрес объекта fileOut
-       writeStream << "1"; // Посылаем строку в поток для записи
-       fileOut.close(); // Закрываем файл
-   }
+//   QFile fileOut("/home/viktor/my_projects_qt_2/cycle_of_distinguishing_a_one_with_vectors_GUI/error.txt"); // Связываем объект с файлом fileout.txt
+//   if(fileOut.open(QIODevice::WriteOnly | QIODevice::Text)) 
+//   { // Если файл успешно открыт для записи в текстовом режиме
+//       QTextStream writeStream(&fileOut); // Создаем объект класса QTextStream
+//       // и передаем ему адрес объекта fileOut
+//       writeStream << "1"; // Посылаем строку в поток для записи
+//       fileOut.close(); // Закрываем файл
+    //}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///     КАК эту процедуру назвать? Предварительный поиск ошибки?
+// 1а. Ставим синапсы на минимум то есть все на 1. Наверное и 201 нейрон надо поставить на 0?
+    // 1. считываем синапсы из файла все по 1
+    // 2. Считывем нейроны из файла
+    // 3. Решаем то есть считаем 201 нейрон
+    // 4. Считаем ошибку
+  // 1b.
+    // 1. считываем синапсы из файла все по 2147483646
+    // 2. Считывем нейроны из файла
+    // 3. Решаем то есть считаем 201 нейрон
+    // 4. Считаем ошибку
+    // то есть проверяем возможно ли вообще убрать ошибку в принципе
+    // c того конца который ближе к уничтожению ошибки начинаем либо прибавлять либо вычитать по 1
+    // node cherrytree: единица vector
+    
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// запускаем эту программу заново а этот экземплят текущий закрываем
 
         // restart:
-        qApp->quit();
-    QProcess::startDetached(qApp->arguments()[0], qApp->arguments());  
+//        qApp->quit();
+//    QProcess::startDetached(qApp->arguments()[0], qApp->arguments());  
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////   
-  //  list_of_neurons[200]=1073741823; 4294967295
-   
-  //  goto f;
-//    setjmp(f);
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////   
-//    if (variable_error<=0) // тут видимо надо менять на если ошибка = или > то на выход то есть ошибка пропадает если становится > 0
-//        goto d;
-//    std::cout << "variable_error = " << variable_error<< std::endl;
-//    std::cout << "Variable error = 1073741824-list_of_neurons[200] = " << 1073741824-list_of_neurons[200]<< std::endl;
-//    std::cout << "list_of_neurons[200]  = " << list_of_neurons[200]  << std::endl;
-//b:
-//    ////////////////////////////////////////////// Solution function ////////////////////////////////////////////////////////////
-//    for ( var = 100; var < 200; ++var) // This is the range of neurons
-//    {
-//        for (int neuron_index = 0, synapse_index = 0;   neuron_index < 200, synapse_index < 10100;   ++neuron_index, synapse_index = synapse_index + 100)
-        
-//        {
-            
-//            list_of_neurons[var]=list_of_neurons[var]+  (list_of_neurons[neuron_index]/ list_of_synapses[synapse_index]);    
-//        }
-//    }
-    
-//    for (int   neuron_index = 100, synapse_index = 10000; neuron_index < 200;   ++neuron_index, ++synapse_index)
-//    {
-//        list_of_neurons[200] = list_of_neurons[200] + (list_of_neurons[neuron_index] / list_of_synapses[synapse_index]);
-//    }
-//    variable_error     =   1073741824-list_of_neurons[200] ;
-//    //     std::cout << "list_of_neurons[201] = " << list_of_neurons[201]<< std::endl;
-//    if (variable_error<=0) // to the exit
-//        goto c;
-//    if (list_of_synapses[10100]==1) // to the exit
-//    {
-//        std::cout << "The values of all synapses are minimal and equal to 1." << std::endl;
-//        goto d;
-//    }
-//e:
-//    if (list_of_synapses[variable_synapse_index_counter] >1)
-//        list_of_synapses[variable_synapse_index_counter]  =  list_of_synapses[variable_synapse_index_counter]-1;
-    
-//    if (list_of_synapses[variable_synapse_index_counter] ==1)
-//    {
-//        variable_synapse_index_counter = variable_synapse_index_counter+1;
-//        goto e;
-//    }
-//    if (variable_synapse_index_counter<10100)
-//        variable_synapse_index_counter=variable_synapse_index_counter+1;
-//    else
-//        variable_synapse_index_counter=0;
-//    goto b;
-//c:
-//    std::cout << "The error has disappeared. Variable error = " << variable_error<< std::endl;
-//    std::cout << "list_of_neurons[200] = " << list_of_neurons[200]<< std::endl;
-//d:
-//    fstream file;
-//    file.open("/home/viktor/my_projects_qt_2/cycle_of_distinguishing_a_one_with_vectors/synapses.txt-4",ios_base::out);
-    
-//    vector<unsigned long long>::iterator itr;
-    
-//    for(itr=list_of_synapses.begin();itr!=list_of_synapses.end();itr++)
-//    {
-//        file<<*itr<<endl;
-//    }
-    
-//    file.close();
-//    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//    //    fstream file2;
-//    //    file2.open("/home/viktor/my_projects_qt_2/cycle_of_distinguishing_a_one_with_vectors/neurons_and_signal.txt",ios_base::out);
-    
-//    //    vector<unsigned long long>::iterator itr2;
-    
-//    //    for(itr2=list_of_neurons.begin();itr2!=list_of_neurons.end();itr2++)
-//    //    {
-//    //        file2<<*itr2<<endl;
-//    //    }
-    
-//    //    file2.close();
-//    std::cout << "The error has disappeared. Variable error = " << variable_error<< ". Это выход. "<<std::endl;
-//            std::cout << "list_of_neurons[200] = " << list_of_neurons[200]<< std::endl;
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+
 }
 
